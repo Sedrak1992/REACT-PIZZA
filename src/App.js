3eebc53e8df032f9a98ea35d9e2debf2028dc9b2
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-07-28 18:01:14 +0400

    ddd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 import Header from "./Content/Header/Header.jsx";
-import { useState } from "react";
 import CardComponent from "./CardComponent/CardComponent.jsx";
 import "./App.css";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Javascript", body: "Description" },
-    { id: 3, title: "Javascript", body: "Description" },
-    { id: 3, title: "Javascript", body: "Description" },
-    { id: 3, title: "Javascript", body: "Description" },
-  ]);
+  const list = [
+    {
+      id: 1,
+      img: "/REACT-PIZZA/PizzaImg/image11.png",
+      title: "Чизбургер-пицца",
+      price: "395",
+    },
+    {
+      id: 2,
+      img: "/REACT-PIZZA/PizzaImg/image22.png",
+      title: "Сырная",
+      price: "450",
+    },
+    {
+      id: 3,
+      img: "/REACT-PIZZA/PizzaImg/image33.png",
+      title: "Креветки по-азиатски",
+      price: "290",
+    },
+    {
+      id: 4,
+      img: "/REACT-PIZZA/PizzaImg/image44.png",
+      title: "Сырный цыпленок",
+      price: "385",
+    },
+    {
+      id: 5,
+      img: "/REACT-PIZZA/PizzaImg/image11.png",
+      title: "Чизбургер-пицца",
+      price: "395",
+    },
+    {
+      id: 6,
+      img: "/REACT-PIZZA/PizzaImg/image22.png",
+      title: "Сырная",
+      price: "450",
+    },
+    {
+      id: 7,
+      img: "/REACT-PIZZA/PizzaImg/image33.png",
+      title: "Креветки по-азиатски",
+      price: "290",
+    },
+    {
+      id: 8,
+      img: "/REACT-PIZZA/PizzaImg/image44.png",
+      title: "Сырный цыпленок",
+      price: "385",
+    },
+  ];
 
   return (
     <div className="App">
       <Header />
-      <CardComponent  posts={id}  title={title} />
+      <CardComponent list={list} />
     </div>
   );
 }
--- src/CardComponent/CardComponent.jsx
+++ src/CardComponent/CardComponent.jsx
@@ -1,10 +1,31 @@
-const CardComponent = ({ posts, title }) => {
+import "./cardComponent.css";
+import React from "react";
+
+const CardComponent = ({ list }) => {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>{title}</h1>
-      {posts.map((post, index) => (
-        <PostItem number={index + 1} key={post.id} post={post} />
-      ))}
+      <h2>Все пиццы</h2>
+      <div className="CardComponent">
+        {list.map((item, id) => (
+          <div key={item.id}>
+            <img src={item.img} />
+            <h4>{item.title}</h4>
+            <div className="div__button">
+              <button>тонкое</button>
+              <button>традиционное</button>
+              <button>26 см.</button>
+              <button>30 см.</button>
+              <button>40 см.</button>
+            </div>
+            <div className="price__button">
+              <div>от {item.price} ₽</div>
+              <div>
+                <button className="add__button">+ Добавить </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
